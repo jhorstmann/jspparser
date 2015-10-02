447d00a095b6c33729d08551a379d1cf9b706404
Jörn Horstmann <git@jhorstmann.net>
2015-10-02 23:10:46 +0200

    Reduced number of mvn incovations in release script

--- release.sh
+++ release.sh
@@ -6,16 +6,10 @@ DEFAULT_VERSION=1.0-SNAPSHOT
 RELEASE_VERSION=${1?"Usage $0 release-version [next-version]"}
 NEXT_VERSION=${2-$DEFAULT_VERSION}
 
-mvn scm:check-local-modification
-
 # release
-mvn versions:set -DnewVersion=$RELEASE_VERSION
-git add pom.xml
-git commit -m "Release $RELEASE_VERSION"
-mvn clean deploy -Prelease
-mvn scm:tag
+mvn scm:check-local-modification versions:set -DnewVersion=$RELEASE_VERSION scm:add -Dincludes=pom.xml scm:checkin -Dmessage="Release $RELEASE_VERSION"
+
+mvn -Prelease clean scm:tag deploy
 
 # next development version
-mvn versions:set -DnewVersion=$NEXT_VERSION
-git add pom.xml
-git commit -m "Develop $NEXT_VERSION"
+mvn versions:set -DnewVersion=$NEXT_VERSION scm:add -Dincludes=pom.xml scm:checkin -Dmessage="Develop $NEXT_VERSION"
